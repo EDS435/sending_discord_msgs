19cdcfc9aff486b8231e0ff032f8a43a7b19e934
Arqam Usman Ali <aa358@njit.edu>
2024-02-06 22:08:47 -0500

    Replaced old code with new code

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,11 +1,30 @@
-'''My Calculator Test'''
-from calculator import add, subtract
+import os
+from dotenv import load_dotenv
+import requests
+import pytest
 
-def test_addition():
-    '''Test that addition function works '''    
-    assert add(2,2) == 4
+load_dotenv()
+discord_token = os.getenv("DISCORD_TOKEN")
 
-def test_subtraction():
-    '''Test that addition function works '''    
-    assert subtract(2,2) == 0
+VALID_CHANNEL_ID = "1201390755604877362"
+VALID_TOKEN = discord_token
+
+@pytest.fixture
+def valid_headers():
+    return {"Authorization": VALID_TOKEN}
+
+def test_send_discord_message_valid(valid_headers):
+    url = f"https://discord.com/api/v9/channels/{VALID_CHANNEL_ID}/messages"
+    payload = {"content": "test"}
+    res = requests.post(url, json=payload, headers=valid_headers)
+
+    assert res.status_code == 200
+
+def test_send_discord_message_invalid_token():
+    url = f"https://discord.com/api/v9/channels/{VALID_CHANNEL_ID}/messages"
+    payload = {"content": "test"}
+    invalid_headers = {"Authorization": "invalid_token"}
+    res = requests.post(url, json=payload, headers=invalid_headers)
+
+    assert res.status_code == 401
     
