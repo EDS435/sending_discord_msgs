848217ab7c6ba06b1c633433b716059390ad791b
Arqam Usman Ali <aa358@njit.edu>
2024-02-13 19:40:09 -0500

    minor fix

--- tests/test_discordmsg.py
+++ tests/test_discordmsg.py
@@ -4,10 +4,9 @@ import requests
 import pytest
 
 load_dotenv()
-discord_token = os.getenv("DISCORD_TOKEN")
-
+VALID_TOKEN = os.getenv("DISCORD_TOKEN")
 VALID_CHANNEL_ID = "1201390755604877362"
-VALID_TOKEN = discord_token
+Content = {"content": "test"}
 
 @pytest.fixture
 def valid_headers():
@@ -15,16 +14,7 @@ def valid_headers():
 
 def test_send_discord_message_valid(valid_headers):
     url = f"https://discord.com/api/v9/channels/{VALID_CHANNEL_ID}/messages"
-    payload = {"content": "test"}
+    payload = Content
     res = requests.post(url, json=payload, headers=valid_headers)
 
     assert res.status_code == 200
-
-def test_send_discord_message_invalid_token():
-    url = f"https://discord.com/api/v9/channels/{VALID_CHANNEL_ID}/messages"
-    payload = {"content": "test"}
-    invalid_headers = {"Authorization": "invalid_token"}
-    res = requests.post(url, json=payload, headers=invalid_headers)
-
-    assert res.status_code == 401
-    
