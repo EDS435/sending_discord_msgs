b0685a03b6a571a1cc09139e42d59669c237ae83
Arqam Usman Ali <aa358@njit.edu>
2024-02-23 15:33:46 -0500

    added ability to send specified no. of messages

--- tests/test_discordmsg.py
+++ tests/test_discordmsg.py
@@ -6,12 +6,16 @@ import pytest
 load_dotenv()
 fake = Faker()
 VALID_TOKEN = os.getenv("DISCORD_TOKEN")
-Content = {"content": fake.word()}
+def pytest_addoption(parser):
+    parser.addoption("--num_records", action="store", type=int, default=1, help="Number of random messages to send")
 @pytest.fixture
 def valid_headers():
     return {"Authorization": VALID_TOKEN}
-def test_send_discord_message_valid(valid_headers):
+@pytest.mark.parametrize("num_records", [2])
+def test_send_discord_message_valid(valid_headers, num_records):
     url = "http://127.0.0.1:5000/api/v1/channels/<channel_id>/messages"
-    payload = Content
-    res = requests.post(url, json=payload, headers=valid_headers)
-    assert res.status_code == 200
+    for _ in range(num_records):
+        Content = {"content": fake.word()}
+        payload = Content
+        res = requests.post(url, json=payload, headers=valid_headers)
+        assert res.status_code == 200
